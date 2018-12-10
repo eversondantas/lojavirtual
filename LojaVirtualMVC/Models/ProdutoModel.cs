1b416bc09ee94dbe47ecccaa0b7d8b97546b4388
Everson Dantas <Everson.dantas@outlook.com>
2018-12-10 14:52:50 -0400

    Model para Produtos e View Index
    Criação de Model para o Produto e da View de listagem de produtos (Index)

---
+++ LojaVirtualMVC/Models/ProdutoModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LojaVirtualMVC.Models
+{
+    public class ProdutoModel
+    {
+        public int ProdutoID { get; set; }
+        public string Codigo { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public int Estoque { get; set; }
+    }
+}
