e885c9756503e9c9089f79b7a941a46dc785378e
Everson Dantas <Everson.dantas@outlook.com>
2018-12-10 16:27:13 -0400

    Criação variável de web API client
    Criação de variável global responsável por consumir a API

---
+++ LojaVirtualMVC/GlobalVariables.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+
+namespace LojaVirtualMVC
+{
+    public static class GlobalVariables
+    {
+        public static HttpClient WebApiClient = new HttpClient();
+
+        static GlobalVariables()
+        {
+            WebApiClient.BaseAddress = new Uri("https://webapi20181210033716.azurewebsites.net/api/");
+            WebApiClient.DefaultRequestHeaders.Clear();
+            WebApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
